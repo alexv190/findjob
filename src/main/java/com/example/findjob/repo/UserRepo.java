212b41aa94d05e0cbfc13be716e444eb9d3c800e
alexv190 <alexv190>
2020-01-02 23:16:12 +0500

    repo fix

--- src/main/java/com/example/findjob/repo/UserRepo.java
+++ src/main/java/com/example/findjob/repo/UserRepo.java
@@ -2,7 +2,9 @@ package com.example.findjob.repo;
 
 import com.example.findjob.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends CrudRepository<User, Long> {
     User findByUsername(String username);
 }
