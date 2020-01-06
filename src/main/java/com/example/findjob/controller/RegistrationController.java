3467436371d92a807bef2645b992b62abeb15baa
alexv190 <alexv190>
2020-01-06 23:51:20 +0500

    added captcha on registration page

--- src/main/java/com/example/findjob/controller/RegistrationController.java
+++ src/main/java/com/example/findjob/controller/RegistrationController.java
@@ -1,23 +1,26 @@
 package com.example.findjob.controller;
 
-import com.example.findjob.domain.Role;
 import com.example.findjob.domain.User;
-import com.example.findjob.repo.UserRepo;
+import com.example.findjob.domain.dto.CaptchaResponseDto;
 import com.example.findjob.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Controller
 public class RegistrationController {
@@ -32,17 +35,24 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(@RequestParam String passwordConfirm,
+                @RequestParam("g-recaptcha-response") String captchaResponse,
                 @Valid User user,
                           BindingResult bindingResult,
                           Model model, @RequestParam Map<String,
             String> form) {
 
-        if (bindingResult.hasErrors()) {
-            model.mergeAttributes(ControllerUtils.getErrors(bindingResult));
-            return "registration";
+        boolean captchaOk = ControllerUtils.checkIfCaptchaOk(captchaResponse);
+
+        if (!captchaOk) {
+            model.addAttribute("captchaError","Заполните капчу!");
         }
+
         if (!StringUtils.equals(user.getPassword(), passwordConfirm)) {
             model.addAttribute("passwordConfirmError","Пароли не совпадают");
+        }
+
+        if (bindingResult.hasErrors() || !captchaOk || !StringUtils.equals(user.getPassword(), passwordConfirm)) {
+            model.mergeAttributes(ControllerUtils.getErrors(bindingResult));
             return "registration";
         }
         String result = userService.addUser(user, form.get("r_role"));
